418db84e894dff399dfcc2cf8ff975ed35757efb
Daniel Rivera <127487333+Danielnvcd@users.noreply.github.com>
2025-04-08 09:36:11 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	
+
+		int suma =0;
+		
+		
+	 for (int k  = 5; k <= 49; k++) {
+		    
+		   suma+= 11 / Math.cbrt(1331);
+		    
+		    
+		  System.out.println(suma+" su binarios es " + Integer.toBinaryString(suma));
+		    
+		   
+		    
+		}
+
+	}
+}
